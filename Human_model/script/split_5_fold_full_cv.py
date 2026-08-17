28c7bb0812e4e094dec672792be843988dcebf5e
yxy <yaoxingyun@westlake.edu.cn>
2026-08-17 23:06:24 +0800

    update for response

--- Human_model/script/split_5_fold_full_cv.py
+++ Human_model/script/split_5_fold_full_cv.py
@@ -18,44 +18,44 @@ def main():
 
     print(f"Loading sample info from {sample_info_path}")
     sample_info = pd.read_csv(sample_info_path, sep='\t', index_col=['seqID'])
-    
+
     print(f"Loading training IDs from {trn_ids_path}")
     with open(trn_ids_path, 'r') as f:
         trn_ids = [line.strip() for line in f if line.strip()]
 
-    trn_info = sample_info.loc[trn_ids]
-    
+    trn_info = sample_info[sample_info.index.isin(trn_ids)]
     seed = 1637
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     print("\nSplitting and saving folds...")
-    
+
     for fold_idx, (train_index, val_index) in enumerate(skf.split(trn_info, trn_info['stage']), 1):
         train_ids = trn_info.index[train_index].tolist()
         val_ids = trn_info.index[val_index].tolist()
 
         train_ids = [x for x in sample_info.index if x in train_ids]
         val_ids = [x for x in sample_info.index if x in val_ids]
-        
+
         train_ids = sorted(train_ids)
         val_ids = sorted(val_ids)
 
         trn_output_filename = f"{exp_name}_trncv_{fold_idx}.trn.ids.txt"
         test_output_filename = f"{exp_name}_trncv_{fold_idx}.test.ids.txt"
-        
+
         trn_output_path = os.path.join(model_data_dir, trn_output_filename)
         test_output_path = os.path.join(model_data_dir, test_output_filename)
-        
+
         with open(trn_output_path, 'w') as f:
             for sample_id in train_ids:
                 f.write(f"{sample_id}\n")
-                
+
         with open(test_output_path, 'w') as f:
             for sample_id in val_ids:
                 f.write(f"{sample_id}\n")
-                
+
         print(f"Saved fold {fold_idx}:")
         print(f"  {trn_output_filename} with {len(train_ids)} samples.")
         print(f"  {test_output_filename} with {len(val_ids)} samples.")
+
 
 if __name__ == "__main__":
     start_time = datetime.now()
---
+++ Human_model/script/step2.py
@@ -0,0 +1,95 @@
+import argparse
+import os
+from datetime import datetime
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from hy.data_loader import load_sample_info, load_separate_cohorts
+from hy.message import message_to_sns
+
+
+def get_location(location):
+    if location == "WORKING_DIR":
+        return args.working_dir
+    elif location == "MODEL_DATA":
+        return os.path.join(args.model_data)
+    elif location == "SCRIPT":
+        return os.path.join(args.script_dir)
+    elif location == "REPORT":
+        return os.path.join(args.working_dir, "results/3_FeatureReduction")
+    else:
+        return os.path.join(args.working_dir, "results")
+
+def main(args):
+    # p100 to p80 and p20
+    sample_info = load_sample_info(get_location("MODEL_DATA"), 'gc')
+    p80_ids_path = os.path.join(get_location("MODEL_DATA") + f"/{args.exp_name}.trn.ids.txt")
+    p80_neg_path = os.path.join(get_location("MODEL_DATA") + f"/{args.exp_name}.neg.ids.txt")
+
+    if file.exists(p80_ids_path):
+        # This is for new added full_cv part
+        p80 = load_separate_cohorts(get_location('MODEL_DATA'), args.exp_name, 'trn')
+        p20 = load_separate_cohorts(get_location('MODEL_DATA'), args.exp_name, 'test')
+    else:
+        seed = 1234
+        # always generate p80_ids_path
+        print(f"generating {args.exp_name}.ids.txt")
+        p100 = load_separate_cohorts(get_location("MODEL_DATA"), args.exp_name, "p100")
+        if p100.index.duplicated().any():
+            message_to_sns(f"{args.exp_name}以下样本ID重复，可能存在配置问题，请检查！\n{p100.index[p100.index.duplicated()]}")
+            exit(1)
+
+        p80, p20, y_p80, y_p20 = train_test_split(sample_info.loc[p100.index],
+                     sample_info.loc[p100.index]['target'],
+                     test_size=0.2,
+                     stratify=sample_info.loc[p100.index]['stage'],
+                     random_state=seed)
+        p80.to_csv(p80_ids_path, sep=',', index=True, header=False, columns=[])
+
+        p80_hd = p80[p80['target'] == 0]
+        p80_hd.to_csv(p80_neg_path, sep=',', index=True, header=False, columns=[])
+        p20.to_csv(get_location("MODEL_DATA") + f"/{args.exp_name}.test.ids.txt", sep=',', index=True, header=False, columns=[])
+    for i in range(1, 51):
+        p64, _, _, _ = train_test_split(sample_info.loc[p80.index],
+                                  sample_info.loc[p80.index]['target'],
+                                  test_size=0.2,
+                                  stratify=sample_info.loc[p80.index]['target'], #这个特意设置成了 target 维持总数
+                                  random_state=i+seed)
+        # 获取 p64 中 target=0 和 target=1 的数量
+        count_0 = (p64['target'] == 0).sum()
+        count_1 = (p64['target'] == 1).sum()
+
+        # 取较少的一个类别数量
+        min_count = min(count_0, count_1)
+
+        # 对 target=0 和 target=1 分别采样 min_count 个样本
+        balanced_p64 = pd.concat([
+            p64[p64['target'] == 0].sample(n=min_count, random_state=i + seed),
+            p64[p64['target'] == 1].sample(n=min_count, random_state=i + seed)
+        ])
+
+        with open(get_location("WORKING_DIR")+f"/all.{args.exp_name}.sample.info.{i}", 'w') as f:
+            f.write(f"{len(balanced_p64)}\n")
+            for index, row in balanced_p64.iterrows():
+                f.write(f"{index} {row['target']} - -2 0 -1\n")
+
+    # 生成 train.tab 文件
+    script_dir = get_location("SCRIPT")
+    model_data_dir = get_location("MODEL_DATA")
+    # 这里是为了生成train.tab文件
+    os.system(f"bash {script_dir}/make_tab.sh {model_data_dir}/{args.exp_name}.trn.ids.txt trim_q30_gcc_10k_cpm {args.working_dir}/train.tab")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("exp_name", help="实验名称 (如 gc hcc)")
+    parser.add_argument('working_dir', help='工作目录')
+    parser.add_argument('script_dir', help='工作目录')
+    parser.add_argument('model_data', help='工作目录')
+    args = parser.parse_args()
+
+    start_time = datetime.now()
+    main(args)
+    end_time = datetime.now()
+    elapsed_time = end_time - start_time
+
+    print(f"程序运行时间: {elapsed_time}")
